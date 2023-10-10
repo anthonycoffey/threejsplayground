c5164b0e6737c0e97e9ac336be6329c82c50fd1e
Anthony Coffey <coffey.j.anthony@gmail.com>
2023-10-09 19:02:31 -0500

    ..

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
-import Image from 'next/image'
+import { MainMenu } from "@/components/MainMenu";
 
 export default function Home() {
   return (
-      <main>
-          <div className="container mx-auto">
-              <div className="flex fle align-center items-center justify-center">
-                  <h1>Hello World</h1>
-              </div>
-          </div>
-      </main>
-  )
+    <main className="">
+      <div className="container mx-auto">
+        <div className="flex align-center items-center justify-between p-4">
+          <h1 className="text-green-400">Hello, World!</h1>
+          <MainMenu />
+        </div>
+        <div className="flex align-center justify-center border-2 border-green-400 px-4 py-6 my-4">
+          Welcome! My name is Anthony Coffey, and I am a full stack software
+          engineer in Austin, Texas
+        </div>
+      </div>
+    </main>
+  );
 }
---
+++ components/MainMenu.tsx
@@ -0,0 +1,13 @@
+// @flow
+import * as React from "react";
+
+type Props = {};
+
+export function MainMenu(props: Props) {
+  return (
+    <div className="">
+      <button className="mr-4">Portfolio</button>
+      <button>Blog</button>
+    </div>
+  );
+}
